5a5d399f943d2fb6a2061721556cf23b75d33e67
Sharla Gelfand <sharla.gelfand@gmail.com>
2023-11-22 10:30:42 -0500

    Initial commit

---
+++ R/app_ui.R
@@ -0,0 +1,34 @@
+#' The application User-Interface
+#'
+#' @param request Internal parameter for `{shiny}`.
+#'     DO NOT REMOVE.
+#' @noRd
+app_ui <- function(request) {
+  shiny::tagList(
+    golem_add_external_resources(),
+    shiny::fluidPage(
+      shiny::actionButton("auth", "Authenticate"),
+      shiny::uiOutput("select_project")
+    )
+  )
+}
+#' Add external Resources to the Application
+#'
+#' This function is internally used to add external
+#' resources inside the Shiny application.
+#'
+#' @importFrom golem add_resource_path activate_js favicon bundle_resources
+#' @noRd
+golem_add_external_resources <- function() {
+  add_resource_path(
+    "www",
+    app_sys("app/www")
+  )
+  shiny::tags$head(
+    favicon(),
+    bundle_resources(
+      path = app_sys("app/www"),
+      app_title = "mermaidrcoralnet"
+    )
+  )
+}
