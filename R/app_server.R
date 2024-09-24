f3a6ee7947620b18f4834cbb32c7d172d9802753
Sharla Gelfand <sharla.gelfand@gmail.com>
2024-09-24 11:14:28 -0400

    Switch to dev

--- R/app_server.R
+++ R/app_server.R
@@ -51,7 +51,7 @@ app_server <- auth0_server(function(input, output, session) {
     aux_mapped = FALSE,
     preview_confirm_shown = 0,
     dev = FALSE,
-    prod = TRUE,
+    prod = FALSE,
     reset = NULL,
     # dev_scenario = "empties"
     # dev_scenario = "wrong_values"
