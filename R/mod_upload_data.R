7b959b3dfbafd5fa99350bb9d71858f56e6284d4
Sharla Gelfand <sharla.gelfand@gmail.com>
2025-07-14 14:59:01 -0400

    Data validation for reefcloud upload

--- R/mod_upload_data.R
+++ R/mod_upload_data.R
@@ -27,9 +27,9 @@ mod_upload_data_server <- function(id, r) {
         if (r$step_select_valid_project_done) {
           shiny::div(
             id = "upload-parent",
-            shiny::h2(get_copy("upload_data", "title")),
+            shiny::h2(get_copy("upload_data", "title", r$provider)),
             spaced(
-              get_copy("upload_data", "text"),
+              get_copy("upload_data", "text", r$provider),
               mod_upload_instructions_ui(ns("instructions")),
               shiny::HTML("</p>")
             ),
@@ -62,25 +62,40 @@ mod_upload_data_server <- function(id, r) {
 
       cols <- names(cols)
 
-      # There are required columns, but the auxiliary columns do not actually have to be named Aux1, Aux2, ..., Aux 5
-      # So look for columns in `required_annotations_columns_start`, then # of columns in `required_annotations_columns_aux`, then `required_annotations_columns_end`
+      provider <- r$provider
 
-      required_annotations_columns_start <- get_config("required_annotations_columns_start")
-      required_annotations_columns_end <- get_config("required_annotations_columns_end")
+      # Required columns ----
+      if (provider == "coralnet") {
+        # There are required columns, but the auxiliary columns do not actually have to be named Aux1, Aux2, ..., Aux 5
+        # So look for columns in `required_annotations_columns_start`, then # of columns in `required_annotations_columns_aux`, then `required_annotations_columns_end`
 
-      contains_known_required_columns <- all(c(required_annotations_columns_start, required_annotations_columns_end) %in% cols)
+        required_annotations_columns_start <- get_config("required_annotations_columns_start")
+        required_annotations_columns_end <- get_config("required_annotations_columns_end")
 
-      if (contains_known_required_columns) {
-        last_start_col <- required_annotations_columns_start[length(required_annotations_columns_start)]
-        last_start_col_index <- which(cols == last_start_col)
-        first_aux_index <- last_start_col_index + 1
-        last_aux_index <- last_start_col_index + get_config("required_annotations_columns_aux")
+        contains_known_required_columns <- all(c(required_annotations_columns_start, required_annotations_columns_end) %in% cols)
 
-        r$auxiliary_columns <- cols[first_aux_index:last_aux_index]
-        r$upload_contains_required_cols <- TRUE
-        r$required_annotations_columns <- c(required_annotations_columns_start, r$auxiliary_columns, required_annotations_columns_end)
-      } else {
-        r$upload_contains_required_cols <- FALSE
+        if (contains_known_required_columns) {
+          last_start_col <- required_annotations_columns_start[length(required_annotations_columns_start)]
+          last_start_col_index <- which(cols == last_start_col)
+          first_aux_index <- last_start_col_index + 1
+          last_aux_index <- last_start_col_index + get_config("required_annotations_columns_aux")
+
+          r$auxiliary_columns <- cols[first_aux_index:last_aux_index]
+          r$upload_contains_required_cols <- TRUE
+          r$required_annotations_columns <- c(required_annotations_columns_start, r$auxiliary_columns, required_annotations_columns_end)
+        } else {
+          r$upload_contains_required_cols <- FALSE
+        }
+      } else if (provider == "reefcloud") {
+        # Check that upload columns match the template columns (rather than listing all of the columns in the config file)
+        reefcloud_template <- readr::read_csv(app_sys(get_config("reefcloud_template_path")), n_max = 0, show_col_types = FALSE)
+        reefcloud_cols <- names(reefcloud_template)
+
+        if (identical(cols, reefcloud_cols)) {
+          r$upload_contains_required_cols <- TRUE
+        } else {
+          r$upload_contains_required_cols <- FALSE
+        }
       }
 
       # If it does not contain the correct columns, show a modal and do not allow them to continue
@@ -89,29 +104,35 @@ mod_upload_data_server <- function(id, r) {
       } else {
         # If it does contain the correct columns, read in the data and proceed
         # Only read in the required columns
-        annotations_raw <- readr::read_delim(input$annotations$datapath, show_col_types = FALSE, col_select = r$required_annotations_columns, delim = r$csv_sep)
 
-        # Check that the Date column is formatted properly - if not, show a modal that there is an issue
-        date_validation <- check_valid_dates(annotations_raw[["Date"]])
-        if (!date_validation[["valid"]]) {
-          mod_upload_instructions_server("instructions_invalid_date", show_ui = FALSE, invalid = TRUE)
-        } else {
-          r$annotations_raw <- annotations_raw
+        if (provider == "coralnet") {
+          annotations_raw <- readr::read_delim(input$annotations$datapath, show_col_types = FALSE, col_select = r$required_annotations_columns, delim = r$csv_sep)
 
-          # Reformat the dates to ymd
-          r$annotations_raw[["Date"]] <- reformat_dates(annotations_raw[["Date"]], date_validation[["format"]])
-
-          # Disable data upload after a single upload - need to reset to change data
-          shinyjs::disable("annotations")
-
-          # Pointer etc of disabling
-          # Disable pointer events on actual button, add style
-          # Not allowed cursor on parent div, add style
-          shinyjs::runjs("document.getElementById('upload-parent').getElementsByClassName('input-group')[0].style.pointerEvents = 'none'; document.getElementById('upload-parent').style.cursor = 'not-allowed';")
-
-          # Flag that valid data has been uploaded
-          r$step_upload_valid_data_done <- TRUE
+          # Check that the Date column is formatted properly - if not, show a modal that there is an issue
+          date_validation <- check_valid_dates(annotations_raw[["Date"]])
+          if (!date_validation[["valid"]]) {
+            mod_upload_instructions_server("instructions_invalid_date", show_ui = FALSE, invalid = TRUE)
+          } else {
+            # Reformat the dates to ymd
+            r$annotations_raw[["Date"]] <- reformat_dates(annotations_raw[["Date"]], date_validation[["format"]])
+          }
+        } else if (provider == "reefcloud") {
+          annotations_raw <- readr::read_delim(input$annotations$datapath, show_col_types = FALSE, delim = r$csv_sep)
+          date_validation <- list(valid = TRUE, format = "ymd")
         }
+
+        r$annotations_raw <- annotations_raw
+
+        # Disable data upload after a single upload - need to reset to change data
+        shinyjs::disable("annotations")
+
+        # Pointer etc of disabling
+        # Disable pointer events on actual button, add style
+        # Not allowed cursor on parent div, add style
+        shinyjs::runjs("document.getElementById('upload-parent').getElementsByClassName('input-group')[0].style.pointerEvents = 'none'; document.getElementById('upload-parent').style.cursor = 'not-allowed';")
+
+        # Flag that valid data has been uploaded
+        r$step_upload_valid_data_done <- TRUE
       }
     }) %>%
       shiny::bindEvent(input$annotations)
